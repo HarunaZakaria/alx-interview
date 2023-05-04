e83d5f8b21f09114d7ca76a04666c255fdd80bc5
harunaZakaria <Harunzy55@gmail.com>
2023-05-04 21:53:53 +0000

    add

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,36 @@
 #!/usr/bin/node
+/*
+script that prints all characters of a Star Wars movie:
+The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+Display one character name by line in the same order of the list “characters” in the /films/ response
+You must use the Star wars API
+You must use the module request
+*/
+
 const request = require('request');
+const movieID = process.argv.slice(2);
+const endpoint = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-const movieID = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieID}/`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
+function makeRequest (array, index) {
+  if (index === array.length) {
     return;
   }
 
-  const movie = JSON.parse(body);
-  const characters = movie.characters;
-
-  console.log(`The characters in ${movie.title} are:`);
-
-  characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
+  request(array[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      makeRequest(array, index + 1);
+    }
   });
+}
+
+request(endpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const chrList = JSON.parse(body).characters;
+    makeRequest(chrList, 0);
+  }
 });
